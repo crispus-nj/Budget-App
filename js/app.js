e72128b22271a2d006b9e48bda9b7ba22e43fd90
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-03 17:43:18 +0300

    added two classes, Budget and html for performing the logic for sorting data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 // variables 
 
+class Budget {
+    constructor(budget){
+        this.budget = Number(budget)
+        this.balance = this.budget
+    }
+}
+class Html{
+    insertAmount(amount){
+        budgetTotal.innerHTML = `${amount}`
+        balance.innerHTML = `${amount}`
+    }
+
+    print_message(message, class_name){
+       const message_wrapper = document.createElement('div')
+        message_wrapper.classList.add('text-center', 'alert', class_name)
+        message_wrapper.appendChild(document.createTextNode(message))
+
+        document.querySelector('.primary').insertBefore(message_wrapper, addExpenseForm)
+
+
+    }
+}
+
+
 const addExpenseForm = document.getElementById('add-expense')
+const budgetTotal = document.querySelector('span#total')
+const balance = document.getElementById('balance')
+
+
+const html = new Html()
 let budget, userBudget;
 
 
@@ -10,11 +39,28 @@ clickFunction = ()=>{
 
     document.addEventListener('DOMContentLoaded', () => {
         userBudget = prompt('What is your monthly budget: \n')
-        console.log("Content loaded!")
+        // console.log(userBudget)
+        if (userBudget === null || userBudget === '' || userBudget === '0'){
+            window.location.reload()
+        } else {
+            // instatiate class Budget
+            budget = new Budget(userBudget)
+            html.insertAmount(budget.budget)
+        }
     })
 
     addExpenseForm.addEventListener('submit', (e)=> {
         e.preventDefault()
+        const expense = document.getElementById('expense')
+        const totalAmount = document.getElementById('amount')
+        const expense_name = expense.value
+        const amount = totalAmount.value
+        if (expense_name === '' || amount === ''){
+            html.print_message('Error occured! All fields are mandatory!', 'alert-danger')
+        } else {
+            console.log("Valid details")
+            console.log(expense_name, amount)
+        }
     })
 }
 
